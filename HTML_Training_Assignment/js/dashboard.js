3e2785c107c80ca95e4cc62029de74a843d759fc
Priyank3117 <priyank.patel@etatvasoft.com>
2024-01-12 18:11:20 +0530

    toggle sidebar

---
+++ HTML_Training_Assignment/js/dashboard.js
@@ -0,0 +1,22 @@
+var sidebar = document.getElementById("sidebar")
+var main = document.getElementById("main-content")
+
+
+
+var flag=1
+function togglesidebar()
+{
+   
+    if(flag == 1)
+    {
+        sidebar.style.display = "none";
+        main.style.width ="100%"
+        flag=0;
+    }
+    else
+    {
+        sidebar.style.display = "flex";
+        main.style.width ="83%"
+        flag=1;
+    }
+}
