4cf0c1bc63b5d492018bd56ff0241b83d970045d
Priyank3117 <priyank.patel@etatvasoft.com>
2024-01-16 15:12:39 +0530

    dashboard responsive added

--- HTML_Training_Assignment/js/dashboard.js
+++ HTML_Training_Assignment/js/dashboard.js
@@ -1,24 +1,30 @@
-var sidebar = document.getElementById("sidebar")
-var main = document.getElementById("main-content")
+var sidebar = document.getElementById("sidebar");
+var main = document.getElementById("main-content");
 
+var flag = 1;
+function togglesidebar() {
+  if (screen.width > 911) {
+    if (flag == 1) {
+      sidebar.style.display = "none";
+      main.style.width = "100%";
+      flag = 0;
+    } else {
+      sidebar.style.display = "flex";
+      main.style.width = "83%";
+      flag = 1;
+    }
+  }
 
-
-var flag=1
-function togglesidebar()
-{
-   
-    if(flag == 1)
-    {
+  else
+  {
+    if (flag == 1) {
         sidebar.style.display = "none";
-        main.style.width ="100%"
-        flag=0;
-    }
-    else
-    {
+        main.style.width = "100%";
+        flag = 0;
+      } else {
         sidebar.style.display = "flex";
-        main.style.width ="83%"
-        flag=1;
-    }
+        main.style.width = "100%";
+        flag = 1;
+      }
+  }
 }
-
-
